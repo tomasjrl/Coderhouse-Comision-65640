a932f928b2e3ed39cf0633574491e56a88d15bc8
tomasjrl <tstabilini@gmail.com>
2024-10-13 00:43:19 -0300

    agrego pagina 404 not found

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ItemListContainer from '../Components/ItemListContainer/ItemListContainer';
 import ItemDetailContainer from '../Components/ItemDetailContainer/ItemDetailContainer';
 import NavBar from '../Components/NavBar/NavBar';
-
+import NotFound from '../Components/NotFound/NotFound'; // Importar componente NotFound
 
 
 const RoutesApp = () => {
@@ -12,7 +12,8 @@ const RoutesApp = () => {
       <Routes>
         <Route path="/" element={<ItemListContainer greeting="Bienvenidos a nuestro e-commerce!" />} />
         <Route path="/category/:categoryId" element={<ItemListContainer />} />
-        <Route path="/item/:itemId" element={<ItemDetailContainer />} /> 
+        <Route path="/item/:itemId" element={<ItemDetailContainer />} />
+        <Route path="*" element={<NotFound />} /> // Ruta 404
       </Routes>
     </BrowserRouter>
   );
